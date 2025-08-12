4af115de6f9276dd3eeab359718b828f31905393
Magnus Edu <99298436+Magnusedu5@users.noreply.github.com>
2025-08-12 17:54:30 +0100

    I Corrected the path for "getresult" and "getdocument" from "api/results/retrieve" to "api/reselts/list and "api/documents/retrieve" to "api/documents/list"

--- DMS_ALX/urls.py
+++ DMS_ALX/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/results/retrieve', views.getresult, name='get_results'),
-    path('api/results/upload/', views.uploadresult, name='upload_result'),
+    path('api/results/list', views.getresult, name='get_results'),
+    path('api/results/upload', views.uploadresult, name='upload_result'),
 
-    path('api/documents/retieve', views.getdocument, name='get_documents'),
-    path('api/documents/upload/', views.uploaddocument, name='upload_document'),
+    path('api/documents/list', views.getdocument, name='get_documents'),
+    path('api/documents/upload', views.uploaddocument, name='upload_document'),
 ]
