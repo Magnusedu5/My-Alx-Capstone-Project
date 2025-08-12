af61f2cc2337f898a6a421ce936958ccea5a1626
Magnus Edu <99298436+Magnusedu5@users.noreply.github.com>
2025-08-12 17:57:51 +0100

    I registered each model to my admin site

--- DMS_ALX/admin.py
+++ DMS_ALX/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Result, Document, Department, CustomUser
 
-# Register your models here.
 admin.site.register(Result)
 admin.site.register(Document)
 admin.site.register(Department)
